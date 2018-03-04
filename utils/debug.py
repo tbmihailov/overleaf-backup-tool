c5270895a2368e2022c03e5382369b8f489df9b0
Todor Mihaylov <tbmihailov@gmail.com>
2018-03-04 23:36:10 +0100

    Refactoring

--- utils/debug.py
+++ utils/debug.py
@@ -37,8 +37,6 @@ def enable_http_client_debug():
     http_client.HTTPConnection.debuglevel = 1
 
     # You must initialize logging, otherwise you'll not see debug output.
-    logging.basicConfig()
-    logging.getLogger().setLevel(logging.DEBUG)
     requests_log = logging.getLogger("requests.packages.urllib3")
     requests_log.setLevel(logging.DEBUG)
     requests_log.propagate = True
